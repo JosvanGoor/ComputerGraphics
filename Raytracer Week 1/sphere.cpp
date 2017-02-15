448d7301d9cb93d6c1fef13b733e8224a070fd1b
Nguyễn Thế Thông <nguyenthethong1996@gmail.com>
2017-02-15 09:35:17 +0100

    Implement sphere.cpp

--- Raytracer Week 1/sphere.cpp
+++ Raytracer Week 1/sphere.cpp
@@ -9,9 +9,9 @@
 //    Maarten Everts
 //    Jasper van de Gronde
 //
-//  This framework is inspired by and uses code of the raytracer framework of 
+//  This framework is inspired by and uses code of the raytracer framework of
 //  Bert Freudenberg that can be found at
-//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
+//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html
 //
 
 #include "sphere.h"
@@ -23,39 +23,60 @@
 Hit Sphere::intersect(const Ray &ray)
 {
     /****************************************************
-    * RT1.1: INTERSECTION CALCULATION
-    *
-    * Given: ray, position, r
-    * Sought: intersects? if true: *t
-    * 
-    * Insert calculation of ray/sphere intersection here. 
-    *
-    * You have the sphere's center (C) and radius (r) as well as
-    * the ray's origin (ray.O) and direction (ray.D).
-    *
-    * If the ray does not intersect the sphere, return false.
-    * Otherwise, return true and place the distance of the
-    * intersection point from the ray origin in *t (see example).
-    ****************************************************/
-
+     * RT1.1: INTERSECTION CALCULATION
+     *
+     * Given: ray, position, r
+     * Sought: intersects? if true: *t
+     *
+     * Insert calculation of ray/sphere intersection here.
+     *
+     * You have the sphere's center (C) and radius (r) as well as
+     * the ray's origin (ray.O) and direction (ray.D).
+     *
+     * If the ray does not intersect the sphere, return false.
+     * Otherwise, return true and place the distance of the
+     * intersection point from the ray origin in *t (see example).
+     ****************************************************/
+    
     // place holder for actual intersection calculation
-
-    Vector OC = (position - ray.O).normalized();
-    if (OC.dot(ray.D) < 0.999) {
-        return Hit::NO_HIT();
+    
+    Vector o = ray.O;
+    Vector d = ray.D;
+    Vector oc = o - position;
+    double b = 2 * oc.dot(d);
+    double c = oc.dot(oc) - r * r;
+    double disc = b * b - 4 * c;
+    double t;
+    
+    if (disc < 0) return Hit::NO_HIT();
+    else {
+        disc = sqrt(disc);
+        double t1 = -b - disc;
+        double t2 = -b + disc;
+        
+        if (t1 < t2) t = t1; else t = t2;
     }
-    double t = 1000;
-
+    
+    /*
+     Vector OC = (position - ray.O).normalized();
+     if (OC.dot(ray.D) < 0.999) {
+     return Hit::NO_HIT();
+     }
+     double t = 1000;
+     */
+    
     /****************************************************
-    * RT1.2: NORMAL CALCULATION
-    *
-    * Given: t, C, r
-    * Sought: N
-    * 
-    * Insert calculation of the sphere's normal at the intersection point.
-    ****************************************************/
-
-    Vector N /* = ... */;
-
+     * RT1.2: NORMAL CALCULATION
+     *
+     * Given: t, C, r
+     * Sought: N
+     *
+     * Insert calculation of the sphere's normal at the intersection point.
+     ****************************************************/
+    
+    Point hit_point = ray.O + t * ray.D;
+    
+    Vector N = (hit_point - position).normalized();
+    
     return Hit(t,N);
 }
